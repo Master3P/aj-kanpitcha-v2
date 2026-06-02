42784593f75e959b5b54ed1fa5c3a4ac0553a352
MasterP <iampaloom10@gmail.com>
2026-06-03 00:02:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,3 +7,5 @@
 const SUPABASE_URL = 'https://xebkfwghtahozjrmpaxq.supabase.co';
 
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_VrqykjqO46ESA8NUNWDdfw_bY5XhTm9';
+
+const BRIDGE_URL = 'https://script.google.com/macros/s/AKfycbwaXJs3-qFeV7ln2btLfx09jTHChMAYvid-spaKnKrqySCl61Lk-qm5PI4VfidFRB5L/exec';
