1f0a718135f1514dd16ff2b3396b9ea8b3bc9335
MasterP <iampaloom10@gmail.com>
2026-06-02 15:31:22 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// =====================================================
+// AJ.Kanpitcha v2 - Supabase Config
+// =====================================================
+// ห้ามใส่ secret key หรือ service_role key ในไฟล์นี้
+// =====================================================
+
+const SUPABASE_URL = 'https://xebkfwghtahozjrmpaxq.supabase.co';
+
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_VrqykjqO46ESA8NUNWDdfw_bY5XhTm9';
